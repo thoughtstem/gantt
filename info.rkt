3bf99b353789272eec41361163a64f94a257af17
Lindsey Handley, Ph.D <lindseyhandley@gmail.com>
2019-10-14 09:10:13 -0700

    Update info.rkt

--- info.rkt
+++ info.rkt
@@ -1,6 +1,6 @@
 #lang info
 (define collection "gantt")
-(define deps '("base" "rsvg"))
+(define deps '("base"  "graph" "rsvg"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/gantt.scrbl" ())))
 (define pkg-desc "Description Here")
