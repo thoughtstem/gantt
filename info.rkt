f80cba190133c48b478144985b77cad0696d25e4
sonnynajar <sonny.najar@gmail.com>
2019-10-15 14:45:10 -0700

    Update info.rkt

--- info.rkt
+++ info.rkt
@@ -1,6 +1,6 @@
 #lang info
 (define collection "gantt")
-(define deps '("base" "graph" "posn"))
+(define deps '("base" "graph" "jack-posn"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/gantt.scrbl" ())))
 (define pkg-desc "Description Here")
